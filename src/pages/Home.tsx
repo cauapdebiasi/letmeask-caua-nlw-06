82ba9a1e7c09386e153982b2e33d90823e3779b7
Caua Debiasi <cauadebiasi@gmail.com>
2021-06-23 23:29:19 -0300

    Database integration and created room logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,11 +9,15 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
+import { FormEvent } from 'react';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export function Home () {
 
     const history = useHistory();
     const {user, signInWithGoogle} = useAuth()
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom(){
         
@@ -23,6 +27,24 @@ export function Home () {
 
         history.push('rooms/new');
 
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+
+        event.preventDefault();
+
+        if (roomCode.trim() === ''){
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()){
+            window.alert('Room does not exist.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -40,10 +62,12 @@ export function Home () {
                         Create room using Google
                     </button>
                     <div className="separator">Or join existent room</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                         type="text"
                         placeholder="Type room code" 
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
                         />
                         <Button type="submit">
                             Join
